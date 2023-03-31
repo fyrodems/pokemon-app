70e20f12201cfffa33a0fa672e25bc399b5ababf
fyrodems <fyrodems@gmail.com>
2023-03-31 22:12:32 +0200

    setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import Navbar from "./sections/Navbar"
 import Wrapper from "./sections/Wrapper"
 import Footer from "./sections/Footer"
 import Background from "./components/Background"
+import Search from "./pages/Search"
 import './scss/index.scss';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import MyList from "./pages/MyList"
+import About from "./pages/About"
+import Compare from "./pages/Compare"
+import Pokemon from "./pages/Pokemon"
 
 const App = () => {
   return (
@@ -12,6 +17,14 @@ const App = () => {
       <BrowserRouter>
       <div className="app">
         <Navbar />
+        <Routes>
+          <Route path="*" element={<Navigate to='pokemon/1' />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/list" element={<MyList />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/compare" element={<Compare />} />
+          <Route path="/pokemon/:id" element={<Pokemon />} />
+        </Routes>
         <Wrapper />
         <Footer />
       </div>
